ee716d90135e286009280eb3bda836cbf0a43e47
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-06-06 22:18:18 +0100

    commit 8

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,10 +5,31 @@ from .serializers import UserSerializer, NoteSerialier
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
+#CREATE_NOTE
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerialier
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+ 
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+            
+#DELETE_NOTE            
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerialier
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
+#CREATE_USER
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer 
